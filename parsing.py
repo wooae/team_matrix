0df3ba604e47ad8c01d5bac7236ce0053505c0d8
Alyssa Woo <wooae@umich.edu>
2016-05-25 05:11:27 -0400

    finished json parsing?

--- parsing.py
+++ parsing.py
@@ -11,30 +11,46 @@ def getHashData(startBlock, endBlock):
 		blockHashURL = "http://api.g-coin.org/api/block-index/" + str(block)
 		response = urllib.urlopen(blockHashURL)
 		jData = json.loads(response.read())
-		print "hash: " + jData["blockHash"]
+#		print "hash: " + jData["blockHash"]
 		blockHashes.append(jData["blockHash"])
-	print blockHashes
+#		print blockHashes
 	return blockHashes
 
-# def getTxnList(blockHashes):
+def getTxnList(blockHashes):
+	"""
+	Find all transactions associated with block hashes
+	"""
 
-"""
-def getJsonData(startBlock, endBlock):
-	for block in range (startBlock, endBlock + 1):
-		blockHashURL = "api.g-coin.org/api/block-index/" + str(block)
-		url = 
-		response = urllib.urlopen(url)
-		data = json.loads(response.read())
-		with open("jdata.json", "a") as myfile:
-			myfile.write(data)
-"""
+	txnList = []
+	for blockHash in blockHashes:
+		blockURL = "http://api.g-coin.org/api/block/" + str(blockHash)
+		response = urllib.urlopen(blockURL)
+		jData = json.loads(response.read())
+		for txn in jData["tx"]:
+			txnList.append(txn)
+			print "transaction: " + txn
+	return txnList
+
+
+def getTxnData(txnList):
+	for txn in txnList:
+		txnURL = "http://api.g-coin.org/api/tx/" + str(txn)
+		response = urllib.urlopen(txnURL)
+		jData = json.loads(response.read())
+		myfile = open("jdata.json", "a")
+		myfile.write(str(jData))
+
 
 if __name__ == "__main__":
 	# Range 1:
-	getHashData(104750, 104779)
+	blockHashes = getHashData(104750, 104751)
+	txnList = getTxnList(blockHashes)
+	getTxnData(txnList)
 
+"""
 	# Range 2:
 	getHashData(153500, 153519)
 
 	# Range 3:
 	getHashData(413165, 413214)
+"""
